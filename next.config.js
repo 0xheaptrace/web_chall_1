f2f73c12e97c5754ec56b3e80c035ee9e4923b0b
Dhruv Thakur <dhruvth4kur@gmail.com>
2026-03-24 01:50:35 +0530

    updated project

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  experimental: {
+    outputFileTracingIncludes: {
+      '/api/resource': ['./secrets/**/*', './assets/**/*'],
+    },
+  },
 }
 
 module.exports = nextConfig
